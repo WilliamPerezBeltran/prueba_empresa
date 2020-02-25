aad255bbb8a6c5166de3891893484cb6fa9b494d
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-02-25 00:43:02 -0500

    add function ListAllDomains without json resonse

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func ReceiveDomainName(ctx *fasthttp.RequestCtx){
     
     //extract data form the domainName key from POST
     domainGet := string(ctx.FormValue("domainName"))
+    InsertDomains(domainGet)
 
     counter_call := 1
 
@@ -128,9 +129,6 @@ func ReceiveDomainName(ctx *fasthttp.RequestCtx){
     // conectDb1()
 }
 
-
-
-
 func webScrapingTitle(domain string){
     url := "https://www."+domain
     response, err := http.Get(url)
@@ -157,7 +155,6 @@ func webScrapingTitle(domain string){
         fmt.Println("No closing tag for title found.")
         os.Exit(0)
     }
-
     // (Optional)
     // Copy the substring in to a separate variable so the
     // variables with the full document data can be garbage collected
@@ -166,7 +163,6 @@ func webScrapingTitle(domain string){
     // Print out the result
     fmt.Printf("Page title: %s\n", pageTitle)
 }
-
 
 func webScrapingLinks(domain string){
      url := "https://www."+domain
@@ -197,7 +193,6 @@ func processElement(index int, element *goquery.Selection) {
         }
     }
 }
-
 
 func conectDb1(){
     db, err := sql.Open("postgres", "postgresql://root@localhost:26257/defaultdb?sslmode=disable")
@@ -256,11 +251,6 @@ func check_if_change(ips int){
         log.Fatal("error connecting to the database: ", err)
     }
 
-    // if _, err := db.Exec("SELECT * FROM server LIMIT $1",ips)
-    // err != nil {
-    //     log.Fatal(err)
-    // }
-
     rows, err := db.Query("SELECT * FROM server LIMIT $1",ips)
 
     if err != nil {
@@ -279,37 +269,57 @@ func check_if_change(ips int){
         }
         fmt.Printf("%d %s %s %s %s %d\n", id, address,ssl_grade,country,owner,call)
     }
-
-
-    // SELECT (id,address,ssl_grade,country,owner,call) FROM server where call = 0;
-    // SELECT * FROM server LIMIT 1;
-
-    // db, err := sql.Open("postgres", "postgresql://root@localhost:26257/defaultdb?sslmode=disable")
-    // if err != nil {
-    //     log.Fatal("error connecting to the database: ", err)
-    // }
-
-    // if _, err := db.Exec("SELECT (address,ssl_grade,country,owner) FROM server where call = 0",address,sslGrade,country,ownwer,call)
-    // err != nil {
-    //     log.Fatal(err)
-    // }
-
-    // for i := 0; i<= (len(ips)-1);i++{
-
-    // }
-
-    // if _, err := db.Exec("INSERT INTO server (address,ssl_grade,country,owner,call) VALUES($1, $2, $3, $4, $5)",address,sslGrade,country,ownwer,call)
-    
-
-
+    // for i := 0; i<= (len(ips)-1);i++{}
 }
 
+func ListAllDomains(ctx *fasthttp.RequestCtx){
+    db, err := sql.Open("postgres", "postgresql://root@localhost:26257/defaultdb?sslmode=disable")
+    if err != nil {
+        log.Fatal("error connecting to the database: ", err)
+    }
 
-var nambersOfIps int 
+    rows, err := db.Query("SELECT * FROM domain_list")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer rows.Close()
+
+    fmt.Println("Initial balances:")
+    
+    for rows.Next() {
+        var id int
+        var domain string
+        if err := rows.Scan(&id, &domain); err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("%d %s\n", id, domain)
+    }
+    ctx.SetContentType("application/json; charset=utf-8")
+}
+
+func InsertDomains(domain string){
+    db, err := sql.Open("postgres", "postgresql://root@localhost:26257/defaultdb?sslmode=disable")
+    if err != nil {
+        log.Fatal("error connecting to the database: ", err)
+    }
+
+    if _, err := db.Exec("INSERT INTO domain_list (domain) VALUES ($1)",domain)
+    err != nil {
+        log.Fatal(err)
+    }
+}
+
+var (
+ nambersOfIps int   
+) 
+// var nambersOfIps int 
 
 func main() {
     router := fasthttprouter.New()
     router.POST("/domain", ReceiveDomainName)
+    router.GET("/listAllDomains", ListAllDomains)
     log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
 }
 
